f1b8c24ecc0b149672133c5671339f8cad1696bd
Heyder Medina <medinaheyder@gmail.com>
2024-07-11 21:48:38 -0600

    final

--- segundo.js
+++ segundo.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('enviar4').addEventListener('click', function () {
+        const dni = parseInt(document.getElementById('dni').value, 10);
+        const answer = document.getElementById('answer4');
+        
+        const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
 
-
-
-}); //FINAL
+        if (dni >= 0 && dni <= 99999999) {
+            const resto = dni % 23;
+            const letra = letras[resto];
+            answer.textContent = `La letra correspondiente a su DNI es: ${letra}`;
+        } else {
+            answer.textContent = 'Inserte un número entre 0 y 99999999';
+        }
+    });
+});
